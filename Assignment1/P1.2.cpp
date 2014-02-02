486b6851a3325cefdb03e7368afa2cc05095aaf9
sudhargk <sudharpun90@gmail.com>
2014-02-02 14:51:46 +0530

    Added 2nd Programme and made changes in P1.1.cpp for debuggin

---
+++ Assignment1/P1.2.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+//#include <cilk/cilk.h>
+void lcs(char* seq1,int pos1,int length1,char *seq2,int pos2,int length2){
+    seq1[pos1]=
+}
+int main(int argc,char**argv){
+    
+   return 0;
+}
